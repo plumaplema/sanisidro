ad3fd9587281d62bc3acb52084d04ab3f7d81482
plumaplema <plumaplema@gmail.com>
2023-01-17 13:27:53 +0800

    update

--- src/pages/api/index.tsx
+++ src/pages/api/index.tsx
@@ -1,33 +1,9 @@
 import { NextApiRequest, NextApiResponse } from "next"
+import prisma from "../../lib/prismadb"
 import transact from "../../lib/transact"
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    if (req.method !== 'POST') {
-        res.status(405).send({ message: 'Only POST requests allowed' })
-        return
-    }
-    const body = JSON.parse(req.body)
+    const student = await prisma.feedBack.findMany()
 
-    const action = [body]
-
-    const result = await transact(action)
-
-    res.status(200).json(result)
+    res.status(200).json(student)
 }
-
-/*{
-        "account": "eosio",
-        "name": "buyram",
-        "data": {
-            "payer": process.env.WALLET,
-            "receiver": process.env.WALLET,
-            "quant": "1.00000000 WAX"
-        },
-        "authorization": [
-            {
-                "actor": "sanisidro123",
-                "permission": "owner"
-            }
-        ]
-    }
-*/
